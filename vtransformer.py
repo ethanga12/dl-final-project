bc38ac8f6b94818c3715fca357dcc62cef9925f6
Ethan Asis <ethan_asis@brown.edu>
2022-05-11 23:03:05 -0400

    more visualization changes

--- vtransformer.py
+++ vtransformer.py
@@ -126,29 +126,6 @@ class Patches(layers.Layer):
         })
         return config
 
-plt.figure(figsize=(4,4))
-image = x_train[np.random.choice(range(x_train.shape[0]))]
-plt.imshow(image.astype("uint8"))
-plt.axis("off")
-
-resized_image = tf.image.resize(
-    tf.convert_to_tensor([image]), size=(image_size, image_size)
-)
-
-patches = Patches(patch_size)(resized_image)
-print(f"Image size: {image_size} X {image_size}")
-print(f"Patch size: {patch_size} X {patch_size}")
-print(f"Patches per image: {patches.shape[1]}")
-print(f"Elements per patch: {patches.shape[-1]}")
-
-n = int(np.sqrt(patches.shape[1]))
-plt.figure(figsize=(4,4))
-for i, patch in enumerate(patches[0]):
-    ax = plt.subplot(n, n, i + 1)
-    patch_img = tf.reshape(patch, (patch_size, patch_size, 3))
-    plt.imshow(patch_img.numpy().astype("uint8"))
-    plt.axis("off")
-
 class PatchEncoder(layers.Layer): 
     def __init__(self, num_patches, projection_dim):
         super(PatchEncoder, self).__init__()
